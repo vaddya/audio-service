6d8eea487bec65484a95324b4bb5eb79294ba85c
vaddya <vadik.dyachkov@gmail.com>
2017-05-31 15:53:04 +0300

    add getplaylist

--- rest/src/main/java/ru/mail/polis/audio_service/rest/controllers/PlaylistsController.java
+++ rest/src/main/java/ru/mail/polis/audio_service/rest/controllers/PlaylistsController.java
@@ -2,6 +2,7 @@ package ru.mail.polis.audio_service.rest.controllers;
 
 import com.google.gson.Gson;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,7 @@ import ru.mail.polis.audio_service.rest.model.Playlist;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
 
 import static java.util.stream.Collectors.toList;
 import static org.springframework.http.HttpStatus.CREATED;
@@ -28,7 +30,7 @@ public class PlaylistsController {
     }
 
     @GetMapping(produces = APPLICATION_JSON_VALUE)
-    public Collection<Playlist> playlists() {
+    public Collection<Playlist> findPlaylists() {
         return db.getCollection("playlists")
                 .find()
                 .into(new ArrayList<>())
@@ -39,7 +41,17 @@ public class PlaylistsController {
     @PostMapping(consumes = APPLICATION_JSON_VALUE)
     @ResponseStatus(CREATED)
     public void createPlaylist(@RequestBody String playlist) {
-        db.getCollection("playlists").insertOne(Document.parse(playlist));
+        db.getCollection("findPlaylist").insertOne(Document.parse(playlist));
+    }
+
+    @GetMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
+    public Playlist findPlaylist(@PathVariable String id) {
+        return db.getCollection("playlists")
+                .find(Filters.eq("id", id))
+                .into(new ArrayList<>())
+                .stream().map(x -> gson.fromJson(x.toJson(), Playlist.class))
+                .findFirst()
+                .orElse(null);
     }
 
 }
